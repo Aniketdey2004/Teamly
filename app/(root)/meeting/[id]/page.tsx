da732af8777508e04ae0de94316d852fce11bb07
Aniket Dey <aniketdey2004@gmail.com>
2026-02-24 11:23:14 +0530

    Stream setup completed, stream provider created and wrapper the application, Meeting setup controls created.

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -1,9 +1,34 @@
-import React from 'react'
+'use client';
+import Loader from '@/components/Loader';
+import MeetingRoom from '@/components/MeetingRoom';
+import MeetingSetup from '@/components/MeetingSetup';
+import { useGetCallById } from '@/hooks/useGetCallById';
+import { useUser } from '@clerk/nextjs';
+import { StreamCall, StreamTheme } from '@stream-io/video-react-sdk';
+import { useState } from 'react';
 
-const Meeting = async ({params}:{params:Promise<{id:string}>}) => {
-    const {id}=await params;
+
+const Meeting = ({params}:{params: {id:string}}) => {
+    const id= params.id;
+
+    const {user, isLoaded}=useUser();
+    const [isSetupComplete,setIsSetupComplete]=useState(false);
+
+    const { call, isCallLoading}=useGetCallById(id);
+
+    if(!isLoaded || isCallLoading)
+      return <Loader/>
+
     return (
-    <div>Meeting Room: #{id}</div>
+      <main className='h-screen w-full'>
+          <StreamCall call={call}>
+            <StreamTheme>
+                {
+                  !isSetupComplete?(<MeetingSetup setIsSetupComplete={setIsSetupComplete}/>):(<MeetingRoom/>)
+                }
+            </StreamTheme>
+          </StreamCall>
+      </main>
   )
 }
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "@/components/ui/sonner"
+import '@stream-io/video-react-sdk/dist/css/styles.css';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -41,6 +43,7 @@ export default function RootLayout({
         }}>
       <body className={`${geistSans.variable} ${geistMono.variable} bg-dark-2`}>
         {children}
+        <Toaster />
       </body>
       </ClerkProvider>
     </html>
